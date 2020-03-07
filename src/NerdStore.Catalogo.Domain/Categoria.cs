684f46a533b0beffac24d3e66517d68c15a4f217
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-03-07 12:14:43 -0300

    feat: adiciona entidade de produto e categoria

---
+++ src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,21 @@
+﻿using NerdStore.Core.DomainObjects;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public string Nome { get; private set; }
+        public string Codigo { get; private set; }
+
+        public Categoria(string nome, string codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+    }
+}
