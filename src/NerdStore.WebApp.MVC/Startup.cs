cdcd48f1d19b1421ace8bf9924d47dc3085fb16a
Carlos Eduardo <carloseduardofox8@gmail.com>
2020-04-12 16:36:03 -0300

    chore: remove https

--- src/NerdStore.WebApp.MVC/Startup.cs
+++ src/NerdStore.WebApp.MVC/Startup.cs
@@ -61,11 +61,8 @@ namespace NerdStore.WebApp.MVC
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            
             app.UseStaticFiles();
 
             app.UseRouting();
